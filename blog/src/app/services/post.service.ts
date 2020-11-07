d85111c79d41fdd8d8def5d620a4d691349ec700
RaulTejero <raultejeromartos@gmail.com>
2020-11-07 17:34:13 +0100

    terminado los servicios

--- blog/src/app/services/post.service.ts
+++ blog/src/app/services/post.service.ts
@@ -6,13 +6,36 @@ import { Post } from '../interfaces/post';
   providedIn: 'root'
 })
 export class PostService {
-  targetsOfBlog: Post[];
+  postsOfBlog: Post[];
 
   constructor() { 
-   
+   this.postsOfBlog = [
+     {
+       title: 'Titulo de prueba',
+       text: 'Texto de prueba',
+       author: 'Autor de prueba',
+       img: 'url prueba',
+       category: 'categoria'
+     }
+   ];
   }
-  addPost(): Promise<Post> {
-    return 
+  addPost(pPost:Post): Promise<Post[]>  {
+    return new Promise((resolve, reject)=> {
+      this.postsOfBlog.push(pPost);
+      resolve(this.postsOfBlog);
+    });
   };
   
+  getAllPost(): Promise<Post[]> {
+    return new Promise<Post[]>((resolve, reject)=> {
+      resolve(this.postsOfBlog);
+    })
+  };
+  getPostByCategory(pCategory): Promise<Post[]> {
+    return new Promise<Post[]>((resolve, reject) => {
+    resolve (this.postsOfBlog.filter(post=>post.category === pCategory));
+    })
+  }
+
 }
+
