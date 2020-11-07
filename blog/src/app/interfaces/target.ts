6e8dd9f0442cd46a617acddb67a1c38281dc6f5b
RaulTejero <raultejeromartos@gmail.com>
2020-11-07 14:50:48 +0100

    rutas terminadas

---
+++ blog/src/app/interfaces/target.ts
@@ -0,0 +1,10 @@
+export interface Target {
+    title?: string,
+    text?:string,
+    author?:string,
+    img?:string,
+    date?: Date,
+    category?: string
+}
+
+// titulo, texto, autor, imagen (puede ser una url), fecha, categoria
