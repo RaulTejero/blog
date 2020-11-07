47f2ace56727a9606194c58a11d1c6af92caf20f
RaulTejero <raultejeromartos@gmail.com>
2020-11-07 14:34:37 +0100

    creadas rutas y trabajando

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { NewComponent } from './new/new.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',pathMatch: 'full', redirectTo:'/blog'},
+  {path:'blog', component: BlogComponent},
+  {path:'new',component: NewComponent},
+  {path:'**',component:BlogComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
