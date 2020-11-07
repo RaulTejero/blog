4dcf68a20cc40c077eca839860b3036a376f60fa
RaulTejero <raultejeromartos@gmail.com>
2020-11-07 11:57:39 +0100

    componentes generados

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { NewComponent } from './new/new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
